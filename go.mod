671a661aa1889ca8f8c3a4b2d210ceab50ef9f86
Florin Pățan <florinpatan@gmail.com>
2019-01-15 12:49:57 +0200

    Final code version

--- go.mod
+++ go.mod
@@ -1,7 +1,12 @@
 module golandtips
 
 require (
+	github.com/bitly/go-hostpool v0.0.0-20171023180738-a3a6125de932 // indirect
+	github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869 // indirect
+	github.com/gocql/gocql v0.0.0-20181124151448-70385f88b28b
 	github.com/jmoiron/sqlx v1.2.0
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/lib/pq v1.0.0
+	github.com/stretchr/testify v1.3.0 // indirect
 	google.golang.org/appengine v1.3.0 // indirect
 )
--- tips.go
+++ tips.go
@@ -11,54 +11,59 @@ package main
 import (
 	"html/template"
 	"io/ioutil"
+	"log"
 	"os"
 
+	"github.com/jmoiron/sqlx"
 	// you must import this package manually, when you type _ " then the completion should show up for the rest of the packages
 	_ "github.com/lib/pq"
 )
 
+type Page struct {
+	Title string
+	Users []User
+}
+
+type User struct {
+	ID       int
+	Username string
+	Job      string
+}
+
 func main() {
+	fileContents, err := ioutil.ReadFile("tpl.gohtml")
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-	// start from typing ReadFile and show how awesome our completion is
-	// - we can show here that we don't need the package to be imported
-	// - show that we can show the quick documentation and quick definition from here
-	// - show that we can use the Example link in the documentation to create a new scratch file
-	// - show that we can create a private gist with the file contents, get the link and share it back to people
-	ioutil.ReadFile("tpl.gohtml")
+	t, err := template.New("Demo").Parse(string(fileContents))
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-	// file is the first return value of the ReadFile call above. show that we can convert string to []byte  using the quickfix
-	template.New("Demo").Parse(file)
+	db, err := sqlx.Open("postgres", "postgres://postgres:postgres@10.0.75.1:5432/goland?sslmode=disable")
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-	// use the sqlxconnect live template
+	err = db.Ping()
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-	// to get this variable created, first go to the page := and then create the struct, assign the User{user} part
-	// then create the User{} and then create the local variable user
 	user := User{}
-	// use .rrv and then replace return with Fatalln(err) - choose log.Fatalln()
+	err = db.Get(&user, "SELECT * FROM users WHERE id = $1", "Florin")
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-	// say that you don't have a database running and you'd like to have one before trying the code
-	// it's a good opportunity to introduce the Docker and (failing) Database plugins
-	// show we support DB query completion with the tables from above
-	db.Get(&user, "SELECT * FROM users WHERE ID = $1", "Florin")
-
-	// first type this type all things below
-	// then use the quickfix to create the missing Page type
 	page := Page{
-		// then create the missing field Title
 		Title: "Demo",
 
-		// Then create the missing type User
-		// Then create the missing field Users
 		Users: []User{user},
 	}
 
-	// use .rrv then same Fatalln
-	t.Execute(os.Stdout, page)
+	if err := t.Execute(os.Stdout, page); err != nil {
+		log.Fatalln(err)
+	}
 }
-
-// after we cover all contents in main(), ask about k8s plugin users
-// show support for quick documentation, completion, and navigate to support
-// show kdep live template in a yaml file
-// show that and helm support (right click on k8s/helm/postgresql/templates/svc.yaml and use Kubernetes | Helm Template)
-
-// then show terraform support
